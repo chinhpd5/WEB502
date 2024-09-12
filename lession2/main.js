f3e55ba19259d5d2e50ed7bf40322a3054fecdc8
chinhpd5 <chinhpd5@fpt.edu.vn>
2024-09-12 20:17:36 +0700

    ts

---
+++ lession2/main.js
@@ -0,0 +1,8 @@
+// main.ts(ts)-> tsc -> main.js(js) -> link -> index.html(live server)
+var myName = "chinhpd5";
+// myName = '50';
+console.log(myName);
+var age = 20;
+var gender = true;
+console.log(age);
+console.log(gender);
---
+++ lession2/main.ts
@@ -0,0 +1,13 @@
+// main.ts(ts)-> tsc -> main.js(js) -> link -> index.html(live server)
+
+
+let myName : string = "chinhpd5";
+// myName = '50';
+console.log(myName);
+
+let age : number = 20;
+let gender : boolean = true;
+
+console.log(age);
+console.log(gender);
+
