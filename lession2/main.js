a385d22bd509c86efd5c345c2137a22dce544549
chinhpd5 <chinhpd5@fpt.edu.vn>
2024-09-14 19:08:51 +0700

    function

--- lession2/main.js
+++ lession2/main.js
@@ -6,10 +6,19 @@
  */
 var myName = "chinhpd5";
 myName = "Phí Đức Chính";
-console.log(myName);
+// console.log(myName);
 var myAge = 20;
-console.log(myAge);
+// console.log(myAge);
 var gender = true;
-console.log(gender);
+// console.log(gender);
 var myAdrress = "Hà Nội";
 // myAdrress = 20
+// function
+function myFunction(name) {
+    return "Xin ch\u00E0o ".concat(name);
+}
+// console.log(myFunction("chinhpd5"));
+function sum(a, b) {
+    console.log(a + b);
+}
+sum(1, 2);
--- lession2/main.ts
+++ lession2/main.ts
@@ -8,15 +8,32 @@
 let myName : string = "chinhpd5";
 myName = "Phí Đức Chính";
 
-console.log(myName);
+// console.log(myName);
 
 let myAge : number = 20
-console.log(myAge);
+// console.log(myAge);
 
 let gender : boolean = true;
-console.log(gender);
+// console.log(gender);
 
 let myAdrress = "Hà Nội";
 // myAdrress = 20
+
+
+// function
+
+function myFunction(name: string): string{
+
+    return `Xin chào ${name}`
+}
+
+// console.log(myFunction("chinhpd5"));
+
+function sum(a: number, b: number): void{
+    console.log(a+b);
+}
+
+sum(1,2)
+
 
 
