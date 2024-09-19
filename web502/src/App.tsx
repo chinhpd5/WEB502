1f46b259c3ada7df8dad01bcac80d9098f483e8a
chinhpd5 <chinhpd5@fpt.edu.vn>
2024-09-19 19:30:59 +0700

    useState

--- web502/src/App.tsx
+++ web502/src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import './App.css'
 import ListProduct from './components/ListProduct'
 import ProductItem from './components/ProductItem'
@@ -31,25 +32,29 @@ function App() {
     }
   ]
 
+  const [count,setCount] = useState<number>(0);
+
+  const handleAdd = ()=>{
+    // tăng count lên 1 đơn vị
+    setCount(count + 1)
+  }
+  const handleRemove = ()=>{
+    // giảm count đi 1 đơn vị
+    // trong setCount có thể nhận lại 1 callback
+    setCount((prev)=>{
+      // prev là giá trước đó của count
+      // console.log(prev);
+      return prev-1;
+    })
+  }
+
   return (
     <>
-
-      <ListProduct listProduct={listProduct} />
-      {/* <ProductItem 
-        name='Tai Nghe Không Dây TWS 5.3' 
-        description='Kết Nối Bluetooth Chống Thấm Nước Có Micro Âm Thanh Nổi Tiện Dụng'
-        image='https://down-vn.img.susercontent.com/file/cn-11134301-7r98o-lpsw5nzzch8580.webp'
-        price={100000}
-        sale={true}
-        />
-
-      <ProductItem 
-        name='Chuột game có dây Logitech G102 ' 
-        description='Tùy chỉnh RGB, 6 nút lập trình, nhẹ'
-        image='https://down-vn.img.susercontent.com/file/ec99d6bd4d34025b8613c81fa815c573.webp'
-        price={150000}
-        sale={false}
-      /> */}
+      <h1>Count: {count}</h1>
+      <button onClick={handleAdd}>Tăng</button>
+      <button onClick={handleRemove}>Giảm</button>
+      <button>Reset</button>
+      
     </>
   )
 }
