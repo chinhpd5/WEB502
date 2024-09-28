1eab810495fdce7e97458d9c43907e5abe6a96fb
chinhpd5 <chinhpd5@fpt.edu.vn>
2024-09-28 20:03:28 +0700

    table

--- web502/src/pages/admin/product/ListProduct.tsx
+++ web502/src/pages/admin/product/ListProduct.tsx
@@ -7,26 +7,55 @@ function ListProduct() {
   // gán vào biến(listProduct) sử useState
 
   //20p: Đổ dữ liệu listProduct ra 1 table
-  const [listProduct,setListProduct] = useState<IProduct[]>([])
+  const [listProduct, setListProduct] = useState<IProduct[]>([]);
 
-  const getAllProduct =async ()=>{
-    const data =await instance.get('/products');
-    if(data.data){
-      setListProduct(data.data)
+  const getAllProduct = async () => {
+    const data = await instance.get("/products");
+    if (data.data) {
+      setListProduct(data.data);
     }
-  }
+  };
 
-  useEffect(()=>{
+  useEffect(() => {
     getAllProduct();
-  },[])
+  }, []);
 
   console.log(listProduct);
-  
 
   return (
     <>
       <h1>Danh sách sản phẩm</h1>
-      
+
+      <table className="table table-bordered table-striped table-hover">
+        <thead>
+          <tr className="table-success">
+            <th scope="col">STT</th>
+            <th scope="col">Tên sản phẩm</th>
+            <th scope="col">Số lượng</th>
+            <th scope="col">Giá bán</th>
+            <th scope="col">Hình Ảnh</th>
+            <th scope="col">Mô tả</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            listProduct?.map((item:IProduct,index)=>{
+              return (
+                <tr key={item.id}>
+                  <th scope="row">{index+1}</th>
+                  <td>{item.title}</td>
+                  <td>{item.stock}</td>
+                  <td>{item.price}</td>
+                  <td><img style={{width:"100px"}} src={item.thumbnail} alt={item.title} /></td>
+                  <td>{item.description}</td>
+                </tr>
+              )
+            })
+          }
+         
+          
+        </tbody>
+      </table>
     </>
   );
 }
