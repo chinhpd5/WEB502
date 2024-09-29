a8fe1e19ad01ac66250e1d12b9bc7ca3266f4db4
chinhpd5 <chinhpd5@fpt.edu.vn>
2024-09-29 07:58:00 +0700

    add giao diện

--- web502/src/interface/product.ts
+++ web502/src/interface/product.ts
@@ -3,14 +3,15 @@ interface IProduct{
     title: string;
     description:string;
     price: number;
-    discountPercentage: number;
-    rating: number;
-    stock: number;
-    brand: string;
-    category:string;
+    discountPercentage?: number;
+    rating?: number;
+    stock?: number;
+    brand?: string;
+    category?:string;
     thumbnail:string;
-    images:string[]
-
+    images?:string[]
 }
+
+export type ProductInput = Omit<IProduct,"id">
 
 export default IProduct;
--- web502/src/pages/admin/product/AddProduct.tsx
+++ web502/src/pages/admin/product/AddProduct.tsx
@@ -1,11 +1,63 @@
+function AddProduct() {
+  return (
+    <>
+      <h1>Thêm mới sản phẩm </h1>
+      <div className="container">
+        <form>
+            {/* Tên sản phẩm */}
+          <div className="mb-3">
+            <label htmlFor="title" className="form-label">
+              Tên sản phẩm
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="title"
+            />
+          </div>
+            {/* giá sản phẩm */}
+          <div className="mb-3">
+            <label htmlFor="price" className="form-label">
+              Giá sản phẩm
+            </label>
+            <input
+              type="number"
+              className="form-control"
+              id="price"
+            />
+          </div>
 
-function AddProduct (){
+          {/* Hình ảnh sản phẩm */}
+          <div className="mb-3">
+            <label htmlFor="thumbnail" className="form-label">
+              Hình ảnh
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="thumbnail"
+            />
+          </div>
 
-    return(
-        <>
-            <h1>Thêm mới sản phẩm </h1>
-        </>
-    )
+          {/* Mô tả sản phẩm */}
+          <div className="mb-3">
+            <label htmlFor="description" className="form-label">
+              Mô tả
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="description"
+            />
+          </div>
+
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </form>
+      </div>
+    </>
+  );
 }
 
 export default AddProduct;
