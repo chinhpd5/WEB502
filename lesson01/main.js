aa30add34ffcda306d4832cb26f2f7e1d8652dc2
chinhpd5 <chinhpd5@fpt.edu.vn>
2026-01-06 17:37:49 +0700

    string, number, boolean

--- lesson01/main.js
+++ lesson01/main.js
@@ -1,2 +1,13 @@
-console.log("Hello world");
-console.log("chinhpd5");
+//string
+var myName = "chinhpd5";
+// myName = '10';
+// console.log(myName);
+console.log(myName.toUpperCase());
+// number
+var myAge = 20;
+var myScore = 5.7;
+var myMath = -10;
+// console.log(myAge.toUpperCase());
+// boolean
+var myGender = true;
+// myGender = 1
--- lesson01/main.ts
+++ lesson01/main.ts
@@ -1,2 +1,17 @@
-console.log("Hello world");
-console.log("chinhpd5");
+//string
+let myName: string = "chinhpd5";
+// myName = '10';
+// console.log(myName);
+console.log(myName.toUpperCase());
+
+
+// number
+let myAge: number = 20;
+let myScore: number = 5.7;
+let myMath: number = -10;
+// console.log(myAge.toUpperCase());
+
+// boolean
+let myGender: boolean = true;
+// myGender = 1
+
