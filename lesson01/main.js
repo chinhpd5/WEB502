80a5cb4beaef9a1c988f7ca2b1071b1b516ee0d0
chinhpd5 <chinhpd5@fpt.edu.vn>
2026-01-06 15:17:14 +0700

    update

---
+++ lesson01/main.js
@@ -0,0 +1,9 @@
+// string;
+var myName = 'chinhpd5';
+console.log(myName);
+// number
+var myAge = 10;
+var score = 9.5;
+var math = -10;
+// boolean
+var myGender = true;
---
+++ lesson01/main.ts
@@ -0,0 +1,11 @@
+// string;
+let myName: string = 'chinhpd5';
+console.log(myName);
+
+// number
+let myAge: number = 10;
+let score: number = 9.5;
+let math = -10;
+
+// boolean
+let myGender: boolean = true;
