072c49c3171488b85ff5d5b47472059ae1bef0f5
chinhpd5 <chinhpd5@fpt.edu.vn>
2026-01-06 15:36:04 +0700

    array, object

--- lesson01/main.js
+++ lesson01/main.js
@@ -7,3 +7,17 @@ var score = 9.5;
 var math = -10;
 // boolean
 var myGender = true;
+// array
+var myArray = ["nam", "Long", "bình"];
+// myArray.push(10);
+var myArray2 = [0, 1, -3, 4.5];
+for (var i = 0; i < myArray.length; i++) {
+    console.log(myArray[i]);
+}
+// object
+var myInfo = {
+    name: 'chinhpd5',
+    age: 10,
+    gender: false
+};
+console.log("T\u00F4i t\u00EAn l\u00E0 ".concat(myInfo.name, ", \n  Tu\u1ED5i: ").concat(myInfo.age, ", \n  Gi\u1EDBi t\u00EDnh: ").concat(myInfo.gender ? 'Nam' : 'Nữ'));
--- lesson01/main.ts
+++ lesson01/main.ts
@@ -9,3 +9,29 @@ let math = -10;
 
 // boolean
 let myGender: boolean = true;
+
+// array
+let myArray: string[] = ["nam", "Long", "bình"];
+// myArray.push(10);
+let myArray2: number[] = [0, 1, -3, 4.5];
+
+for(let i: number = 0; i< myArray.length; i++){
+  console.log(myArray[i]);
+}
+
+// object
+let myInfo: {
+  name: string,
+  age: number,
+  gender: boolean
+} = {
+  name: 'chinhpd5',
+  age: 10,
+  gender: false
+}
+
+console.log(
+  `Tôi tên là ${myInfo.name}, 
+  Tuổi: ${myInfo.age}, 
+  Giới tính: ${myInfo.gender ? 'Nam': 'Nữ'}`
+);
