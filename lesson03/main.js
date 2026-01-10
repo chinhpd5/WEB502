9d83c925cf9664767b5f8d015a75dccf01bc960a
chinhpd5 <chinhpd5@fpt.edu.vn>
2026-01-10 17:23:36 +0700

    access modifier, read-only

--- lesson03/main.js
+++ lesson03/main.js
@@ -1,17 +1,21 @@
 "use strict";
 class Person {
-    id;
-    name;
-    gender;
+    id; // private: chỉ có thể truy xuất bên trong class
+    name; // public: có thể truy xuất bên trong và ngoài class
+    gender; // protected: chỉ có thể truy xuất bên trong call hoặc class con kế thừa
     constructor(id, name, gender) {
         this.id = id;
         this.name = name;
         this.gender = gender;
     }
+    // nếu không khai báo thì mặc định: public
     showInfo() {
         return `Id: ${this.id}, Họ và tên: ${this.name}, Giới tính: ${this.gender ? 'Nam' : "Nữ"}`;
     }
 }
 const person1 = new Person(1, 'chinhpd5', true);
+// person1.name = 'new chinhpd5' // lỗi vì name: read-only
 console.log(person1.name);
 console.log(person1.showInfo());
+// console.log(person1.id); // lỗi do id: private
+// console.log(person1.gender); // lỗi do gender: protected
--- lesson03/main.ts
+++ lesson03/main.ts
@@ -1,7 +1,7 @@
 class Person{
-  id: number
-  name: string
-  gender: boolean
+  private id: number // private: chỉ có thể truy xuất bên trong class
+  public readonly name: string // public: có thể truy xuất bên trong và ngoài class
+  protected gender: boolean // protected: chỉ có thể truy xuất bên trong call hoặc class con kế thừa
 
   constructor(id: number, name: string, gender: boolean){
     this.id = id;
@@ -9,6 +9,7 @@ class Person{
     this.gender = gender;
   }
 
+  // nếu không khai báo thì mặc định: public
   showInfo():string {
     return `Id: ${this.id}, Họ và tên: ${this.name}, Giới tính: ${this.gender? 'Nam':"Nữ"}`
   }
@@ -16,7 +17,14 @@ class Person{
 
 const person1: Person = new Person(1, 'chinhpd5',true);
 
+// person1.name = 'new chinhpd5' // lỗi vì name: read-only
 console.log(person1.name);
 console.log(person1.showInfo());
+
+// console.log(person1.id); // lỗi do id: private
+
+// console.log(person1.gender); // lỗi do gender: protected
+
+
 
 
