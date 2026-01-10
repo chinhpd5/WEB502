c7988499b709e255a83cfdf17453152bc3a579d3
chinhpd5 <chinhpd5@fpt.edu.vn>
2026-01-10 17:38:49 +0700

    tính kế thừa, tính đóng gói

--- lesson03/main.js
+++ lesson03/main.js
@@ -12,6 +12,13 @@ class Person {
     showInfo() {
         return `Id: ${this.id}, Họ và tên: ${this.name}, Giới tính: ${this.gender ? 'Nam' : "Nữ"}`;
     }
+    getID() {
+        return this.id;
+    }
+    setID(id) {
+        if (id > 0)
+            this.id = id;
+    }
 }
 const person1 = new Person(1, 'chinhpd5', true);
 // person1.name = 'new chinhpd5' // lỗi vì name: read-only
@@ -19,3 +26,19 @@ console.log(person1.name);
 console.log(person1.showInfo());
 // console.log(person1.id); // lỗi do id: private
 // console.log(person1.gender); // lỗi do gender: protected
+// tính đóng gói
+person1.setID(2);
+console.log(person1.getID());
+// tính kế thừa
+class Student extends Person {
+    major;
+    GPA;
+    constructor(id, name, gender, major, GPA) {
+        super(id, name, gender);
+        this.major = major;
+        this.GPA = GPA;
+    }
+}
+const student1 = new Student(2, "chinhpd6", false, 'Công nghệ thông tin', 8.5);
+console.log(student1.name);
+console.log(student1.showInfo());
--- lesson03/main.ts
+++ lesson03/main.ts
@@ -13,6 +13,15 @@ class Person{
   showInfo():string {
     return `Id: ${this.id}, Họ và tên: ${this.name}, Giới tính: ${this.gender? 'Nam':"Nữ"}`
   }
+
+  getID(): number{
+    return this.id;
+  }
+
+  setID(id: number): void {
+    if(id > 0)
+      this.id = id
+  }
 }
 
 const person1: Person = new Person(1, 'chinhpd5',true);
@@ -24,6 +33,27 @@ console.log(person1.showInfo());
 // console.log(person1.id); // lỗi do id: private
 
 // console.log(person1.gender); // lỗi do gender: protected
+
+// tính đóng gói
+person1.setID(2)
+console.log(person1.getID());
+
+// tính kế thừa
+class Student extends Person{
+  major: string
+  GPA: number
+
+  constructor(id: number, name: string, gender: boolean,major: string, GPA: number){
+    super(id,name,gender);
+    this.major = major;
+    this.GPA = GPA;
+  }
+}
+
+
+const student1: Student = new Student(2,"chinhpd6", false,'Công nghệ thông tin', 8.5);
+console.log(student1.name);
+console.log(student1.showInfo());
 
 
 
