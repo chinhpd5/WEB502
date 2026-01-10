913f69570f0c0c011d09579ea041a938ea9636f6
chinhpd5 <chinhpd5@fpt.edu.vn>
2026-01-10 15:27:10 +0700

    tính đóng gói, kế thừa

--- lesson03/main.js
+++ lesson03/main.js
@@ -1,8 +1,8 @@
 "use strict";
 class Person {
-    id;
-    name;
-    gender;
+    id; // private: chỉ có thể truy xuất bên trong class
+    name; // public: có thể truy xuất bên trong hoặc ngoài class và class con kế thừa
+    gender; // protected: chỉ có thể truy xuất bên trong class hoặc class con kế thừa
     constructor(id, name, gender) {
         this.id = id;
         this.name = name;
@@ -11,9 +11,33 @@ class Person {
     showInfo() {
         return `Id: ${this.id},Họ và tên: ${this.name}, Giới tính: ${this.gender ? 'Nam' : 'Nữ'}`;
     }
+    getId() {
+        return this.id;
+    }
+    setId(id) {
+        if (id > 0) {
+            this.id = id;
+        }
+    }
 }
 const person1 = new Person(1, 'chinhpd5', true);
 // person1.name = 'new chinhpd5'; // lỗi vì name: readonly
-console.log(person1.name);
+// console.log(person1.name);
 // console.log(person1.id); lỗi vì id: private
-console.log(person1.showInfo());
+// console.log(person1.showInfo());
+// tính đóng gói: private, protected
+person1.setId(-10);
+console.log(person1.getId());
+// kế thừa
+class Student extends Person {
+    major;
+    GPA;
+    constructor(id, name, gender, major, GPA) {
+        super(id, name, gender);
+        this.major = major;
+        this.GPA = GPA;
+    }
+}
+const student1 = new Student(2, 'chinhpd6', false, 'Công nghệ thông tin', 8.5);
+console.log(student1.GPA);
+console.log(student1.showInfo());
--- lesson03/main.ts
+++ lesson03/main.ts
@@ -1,7 +1,7 @@
 class Person {
-  private id: number
-  public readonly name: string
-  protected gender: boolean
+  private id: number // private: chỉ có thể truy xuất bên trong class
+  public readonly name: string // public: có thể truy xuất bên trong hoặc ngoài class và class con kế thừa
+  protected gender: boolean // protected: chỉ có thể truy xuất bên trong class hoặc class con kế thừa
 
   constructor(id: number, name: string, gender: boolean){
     this.id = id;
@@ -12,12 +12,45 @@ class Person {
   showInfo():string { // public
     return `Id: ${this.id},Họ và tên: ${this.name}, Giới tính: ${this.gender ? 'Nam' : 'Nữ'}`
   }
+
+  getId(): number {
+    return this.id;
+  }
+
+  setId(id: number): void {
+    if(id > 0){
+      this.id = id
+    }
+  }
+
 }
 
 const person1: Person = new Person(1,'chinhpd5',true);
 // person1.name = 'new chinhpd5'; // lỗi vì name: readonly
-console.log(person1.name);
+// console.log(person1.name);
 // console.log(person1.id); lỗi vì id: private
 
-console.log(person1.showInfo());
+// console.log(person1.showInfo());
+
+// tính đóng gói: private, protected
+person1.setId(-10)
+console.log(person1.getId());
+
+
+// kế thừa
+class Student extends Person {
+  major: string
+  GPA: number
+
+  constructor(id: number, name: string, gender: boolean, major: string, GPA: number){
+    super(id, name, gender);
+    this.major = major;
+    this.GPA = GPA
+  }
+}
+
+const student1: Student = new Student(2,'chinhpd6', false, 'Công nghệ thông tin', 8.5);
+
+console.log(student1.GPA);
+console.log(student1.showInfo());
 
