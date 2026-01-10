17c04d79c91d6352e7926e6105f93ce716e84c95
chinhpd5 <chinhpd5@fpt.edu.vn>
2026-01-10 14:57:05 +0700

    class

---
+++ lesson03/main.js
@@ -0,0 +1,17 @@
+"use strict";
+class Person {
+    id;
+    name;
+    gender;
+    constructor(id, name, gender) {
+        this.id = id;
+        this.name = name;
+        this.gender = gender;
+    }
+    showInfo() {
+        return `Id: ${this.id},Họ và tên: ${this.name}, Giới tính: ${this.gender ? 'Nam' : 'Nữ'}`;
+    }
+}
+const person1 = new Person(1, 'chinhpd5', true);
+console.log(person1.name);
+console.log(person1.showInfo());
