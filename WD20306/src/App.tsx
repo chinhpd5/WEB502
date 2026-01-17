d538115d497544e35e52ce12f8089e8092c30f39
chinhpd5 <chinhpd5@fpt.edu.vn>
2026-01-17 15:02:10 +0700

    props2

--- WD20306/src/App.tsx
+++ WD20306/src/App.tsx
@@ -1,13 +1,23 @@
 // import Tailwind from "./components/Tailwind"
+import Button from "./components/Button"
 import ShowInfo from "./components/ShowInfo"
 
 function App() {
 
+  const handleClick= ():void => {
+    console.log("Click!!!");
+  }
+
   return (
     <>
       {/* <Tailwind/> */}
-      <ShowInfo name="chinhpd5" age={30} major="Công nghệ thông tin"/>
-      <ShowInfo name="Nguyễn Văn A" age={21} major="Marketing" />
+      {/* <ShowInfo name="chinhpd5" age={30} major="Công nghệ thông tin"/>
+      <ShowInfo name="Nguyễn Văn A" age={21} major="Marketing" /> */}
+
+      <Button onClick={handleClick} color="red">Click me</Button>
+      <Button color="blue">Click me</Button>
+      <Button color="green">Click me</Button>
+      <Button color="yellow">Click me</Button>
     </>
   )
 }
---
+++ WD20306/src/components/Button.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+type PropsType = {
+  children: React.ReactNode,
+  color: string,
+  onClick?: () => void // có thể null
+}
+
+const renderColor = (color: string): string => {
+  switch(color){
+    case 'red':
+      return 'bg-red-500 hover:bg-red-600';
+    case 'blue':
+      return 'bg-blue-500 hover:bg-blue-600'
+    case 'green':
+      return 'bg-green-500 hover:bg-green-600'
+    case 'yellow':
+      return 'bg-yellow-500 hover:bg-yellow-600'
+    default:
+      return 'bg-blue-500 hover:bg-blue-600'
+  }
+}
+
+function Button(props: PropsType) {
+  return (
+    <button onClick={props.onClick} className={`px-4 py-2 rounded text-white min-w-20 cursor-pointer ${renderColor(props.color)}`}>
+      {props.children}
+    </button>
+  )
+}
+
+export default Button
