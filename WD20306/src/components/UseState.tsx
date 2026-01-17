293b22ea4e540acdf4343a67584e78f5b5ed585e
chinhpd5 <chinhpd5@fpt.edu.vn>
2026-01-17 15:46:48 +0700

    two way binding

--- WD20306/src/components/UseState.tsx
+++ WD20306/src/components/UseState.tsx
@@ -3,14 +3,30 @@ import Button from './Button'
 
 function UseState() {
 
-  const [count, setCount] = useState<number>(0)
+  const [count, setCount] = useState<number>(0);
+  const [text, setText] = useState<string>('abc')
 
   return (
     <div>
-      <h1>Count: {count}</h1>
-      <Button onClick={()=>{setCount(count+1)}}>Tăng</Button>
-      <Button color='red' onClick={()=>{setCount(count-1)}}>Giảm</Button>
-      <Button color='yellow' onClick={()=>{setCount(0)}}>Reset</Button>
+      <div className='border rounded p-4 m-4'>
+        <h1>Count: {count}</h1>
+        <Button onClick={()=>{
+          setCount((prev) => prev+1)
+          setCount((prev) => prev+1)
+          setCount((prev) => prev+1)
+          }}>Tăng</Button>
+        <Button color='red' onClick={()=>{setCount(count-1)}}>Giảm</Button>
+        <Button color='yellow' onClick={()=>{setCount(0)}}>Reset</Button>
+      </div>
+
+      <div className='border rounded p-4 m-4'>
+          {/* two-way binding */}
+          <h1>{text}</h1>
+          <input className='border' type="text" 
+            onChange={(e)=>{setText(e.target.value)}} 
+            value={text}
+          />
+      </div>
     </div>
   )
 }
