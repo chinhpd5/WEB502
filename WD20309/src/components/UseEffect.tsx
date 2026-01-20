996325d52aa80f2f372ff09292b0d92a2fd64dde
chinhpd5 <chinhpd5@fpt.edu.vn>
2026-01-20 17:17:46 +0700

    useEffect v2

--- WD20309/src/components/UseEffect.tsx
+++ WD20309/src/components/UseEffect.tsx
@@ -1,17 +1,33 @@
+import axios from 'axios';
 import React, { useEffect, useState } from 'react'
+import type { IProduct } from '../interfaces/Product';
 
 function UseEffect() {
-  const [text, setText] = useState<string>('')
+  const [text, setText] = useState<string>('');
+  const [products, setProducts] = useState<IProduct[]>([])
 
   // callback được gọi mỗi khi componet re-render
   useEffect(()=>{
-    console.log('re-render');
+    // console.log('re-render');
     document.title = text;
   })
 
   // callback chỉ gọi 1 lần duy nhất khi component render
   useEffect(()=>{
-    console.log('re-render2');
+    const getAllProducts = async () => {
+      try {
+        const {data} = await axios.get(`http://localhost:3000/products`)
+        // console.log(data);
+        if(data){
+          setProducts(data)
+        }
+            
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    getAllProducts();
   },[])
 
   return (
@@ -19,6 +35,19 @@ function UseEffect() {
       <input type="text" className='border' onChange={(e)=>{
         setText(e.target.value)
       }} value={text}/>
+
+      <hr />
+      <h1>Danh sách sản phẩm</h1>
+      <ul>
+        {products && products.map(
+          (item:IProduct,index: number)=>{
+            return (
+            <li key={item.id}>
+              {index+1}| {item.name}| {item.category}
+            </li>)
+          }
+        )}
+      </ul>
     </div>
   )
 }
