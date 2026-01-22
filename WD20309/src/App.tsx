5c8a9cd60b5c7c4959a7819d9038d47cc16866e2
chinhpd5 <chinhpd5@fpt.edu.vn>
2026-01-22 17:17:37 +0700

    layout

--- WD20309/src/App.tsx
+++ WD20309/src/App.tsx
@@ -1,19 +1,29 @@
 import { Route, Routes } from "react-router-dom"
 import Home from "./pages/Home"
 import Product from "./pages/Product"
+import ClientLayout from "./layouts/ClientLayout"
+import AdminLayout from "./layouts/AdminLayout"
+import ProductDetail from "./pages/ProductDetail"
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/product" element={<Product/>}/>
-        <Route path="/about" element={<h1>Trang About</h1>}/>
-        <Route path="/contact" element={<h1>Trang Liên hệ</h1>}/>
-        <Route path="/news" element={<h1>Trang tin tức</h1>}/>
+
+        <Route path="/" element={<ClientLayout/>}>
+          <Route path="" element={<Home/>}/>
+          <Route path="product" element={<Product/>}/>
+          <Route path="product/:id" element={<ProductDetail/>}/>
+          <Route path="about" element={<h1>Trang About</h1>}/>
+          <Route path="contact" element={<h1>Trang Liên hệ</h1>}/>
+          <Route path="news" element={<h1>Trang tin tức</h1>}/>
+        </Route>
+
+        <Route path="/admin" element={<AdminLayout/>}>
+          {/* Route của quản trị viên */}
+        </Route>
 
         <Route path="*" element={<h1>Not found</h1>}/>
-
       </Routes>
     </>
   )
